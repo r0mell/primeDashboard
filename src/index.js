dffa9da54fdf9f99a905f7b9c70e87c12d9108cb
Rommel Pumisacho <nando_db@hotmail.es>
2022-01-18 17:21:04 -0500

    [FIX] añadir caracteristicas

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import {Auth0Provider} from '@auth0/auth0-react'
 import App from './routes/App';
 
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+   <React.StrictMode>
+      <App />
+   </React.StrictMode>
+   , document.getElementById('app'));
