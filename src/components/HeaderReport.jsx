6fa087c25212f6da5525b66f99efb24009ede2bd
Rommel Pumisacho <nando_db@hotmail.es>
2022-04-13 00:26:38 -0500

    [ADD] Mis compras Table

--- src/components/HeaderReport.jsx
+++ src/components/HeaderReport.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import logoDark from '../assets/icons/logo-dark.svg'
+/* import logoDark from '../assets/icons/logo-dark.svg' */
+import logoUnick from '../assets/icons/Unick_logo.png'
 
 function HeaderReport() {
    return (
 
       <div className="layout-topbar">
          <Link to="/" className="layout-topbar-logo">
-            <img src={logoDark} alt="logo" />
-            <span>Unick</span>
+            <img src={logoUnick} alt="logo" />
+            {/* <span>Unick</span> */}
          </Link>
 
          <Link to='/admin/checkout'>
             <button className='reportorder-button'>Regresar</button>
          </Link>
 
-         <button className='reportorder-button'>Descargar</button>
+        {/*  <button className='reportorder-button'>Descargar</button> */}
       </div>
    )
 }
